e4aa1c697762f128f13a2e527fe5728c7e602ffc
reza_bakhshande <reza.bakhshande9574@gmail.com>
2021-03-06 14:07:17 +0330

    component name changed

--- app/src/main/java/com/macroid/bluetoothmessenger/di/components/I_ChatActivityComponent.java
+++ app/src/main/java/com/macroid/bluetoothmessenger/di/components/I_ChatActivityComponent.java
@@ -2,15 +2,13 @@ package com.macroid.bluetoothmessenger.di.components;
 
 
 import com.macroid.bluetoothmessenger.C_ChatActivity;
-import com.macroid.bluetoothmessenger.C_DeviceListActivity;
 import com.macroid.bluetoothmessenger.di.models.C_BluetoothModelDagger;
 import com.macroid.bluetoothmessenger.di.models.C_PermissionHelperModelDagger;
-import com.macroid.bluetoothmessenger.di.models.C_RecyclerViewModelDagger;
-import com.macroid.bluetoothmessenger.di.models.ContextModel;
+import com.macroid.bluetoothmessenger.di.models.C_ContextModel;
 import com.macroid.bluetoothmessenger.di.scope.MainScope;
 
 @MainScope
-@dagger.Component(modules = {C_BluetoothModelDagger.class, C_PermissionHelperModelDagger.class, ContextModel.class})
+@dagger.Component(modules = {C_BluetoothModelDagger.class, C_PermissionHelperModelDagger.class, C_ContextModel.class})
 public interface I_ChatActivityComponent
 {
 
