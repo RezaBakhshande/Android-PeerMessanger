12083eb5c91c0df061fc13fe122934ced44d9929
reza_bakhshande <reza.bakhshande9574@gmail.com>
2021-03-09 02:06:05 +0330

    dagger scope

---
+++ app/src/main/java/com/macroid/bluetoothmessenger/di/scope/MainScope.java
@@ -0,0 +1,12 @@
+package com.macroid.bluetoothmessenger.di.scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface MainScope
+{
+}
