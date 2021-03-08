a1ba1c4beec0d90b61d0752155519a433f38a200
reza_bakhshande <reza.bakhshande9574@gmail.com>
2021-03-08 14:05:44 +0330

    i create this moduels for inject RecyclerView for availbale devices

---
+++ app/src/main/java/com/macroid/bluetoothmessenger/di/models/C_RecyclerViewAvailableDeviceModelDagger.java
@@ -0,0 +1,34 @@
+package com.macroid.bluetoothmessenger.di.models;
+
+import com.macroid.bluetoothmessenger.adapters.C_AvailableDeviceRecyclerViewAdapter;
+import com.macroid.bluetoothmessenger.adapters.I_OnItemListener;
+import com.macroid.bluetoothmessenger.di.scope.MainScope;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class C_RecyclerViewAvailableDeviceModelDagger
+{
+
+    I_OnItemListener onItemListener;
+
+    public C_RecyclerViewAvailableDeviceModelDagger(I_OnItemListener onItemListener)
+    {
+        this.onItemListener = onItemListener;
+    }
+
+    @MainScope
+    @Provides
+    public C_AvailableDeviceRecyclerViewAdapter F_GetAvailableRecyclerView(I_OnItemListener onItemListener)
+    {
+        return new C_AvailableDeviceRecyclerViewAdapter(onItemListener);
+    }
+
+
+    public I_OnItemListener F_GetonItemListener()
+    {
+        return onItemListener;
+    }
+    
+}
