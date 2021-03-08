a91b7c3b37544955f8ac6c6cfd3855f7159ed13a
reza_bakhshande <reza.bakhshande9574@gmail.com>
2021-03-08 14:00:36 +0330

    added C_RecyclerViewAvailableDeviceModelDagger.class to component moduels

--- app/src/main/java/com/macroid/bluetoothmessenger/di/components/I_DeviceListComponent.java
+++ app/src/main/java/com/macroid/bluetoothmessenger/di/components/I_DeviceListComponent.java
@@ -2,12 +2,13 @@ package com.macroid.bluetoothmessenger.di.components;
 
 import com.macroid.bluetoothmessenger.C_DeviceListActivity;
 import com.macroid.bluetoothmessenger.di.models.C_BluetoothModelDagger;
-import com.macroid.bluetoothmessenger.di.models.C_RecyclerViewModelDagger;
 import com.macroid.bluetoothmessenger.di.models.C_ContextModel;
+import com.macroid.bluetoothmessenger.di.models.C_RecyclerViewAvailableDeviceModelDagger;
+import com.macroid.bluetoothmessenger.di.models.C_RecyclerViewModelDagger;
 import com.macroid.bluetoothmessenger.di.scope.MainScope;
 
 @MainScope
-@dagger.Component(modules = {C_RecyclerViewModelDagger.class, C_BluetoothModelDagger.class, C_ContextModel.class})
+@dagger.Component(modules = {C_RecyclerViewModelDagger.class, C_RecyclerViewAvailableDeviceModelDagger.class, C_BluetoothModelDagger.class, C_ContextModel.class})
 public interface I_DeviceListComponent
 {
 
